92e2e4d507462f6ffc9f453dd1cdc485fa9defa0
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-10-23 07:18:15 +0000

    advance js

--- 8_advance_js/script.js
+++ 8_advance_js/script.js
@@ -142,3 +142,58 @@ Rectangle.prototype.getArea = function() {
   return this.width * this.height;
 };
 
+
+/* THIS and Binding Context
+Task 1: Bind the Correct Context
+Create an object person with properties name and a method introduce().
+Use the bind() method to ensure the method works correctly 
+when passed to another function.
+*/
+
+// Task 1
+const person = {
+    name: "Alice",
+    introduce() {
+        return  `Hi, my name is ${this.name}`;
+    }
+};
+const boundIntroduce = person.introduce.bind(person);
+
+
+/*
+Task 2: Using call() to Invoke a Function with Different Contexts
+Write a function introduce() that uses the this keyword to introduce
+a person by name. Then, invoke introduce() using call() to introduce 
+different people with the same function.
+*/
+
+// Task 2
+function introduce() {
+    return `Hi, my name is ${this.name}`;
+}
+ 
+const person1 = { name: 'sanju' };
+const person2 = { name: 'shri' };
+
+introduce.call(person1);
+introduce.call(person2);
+
+
+/*
+Task 3: Using apply() to Pass Arguments with Context
+Create a function sum() that accepts two numbers and
+uses this to access a multiplier value. Then, invoke sum()
+with different contexts using apply(), passing the numbers as an array.
+*/
+
+// Task 3
+function sum(a, b) {
+     return (a + b) * this.multiplier;
+}
+ 
+const context1 = { multiplier: 2 };
+const context2 = { multiplier: 3 };
+ 
+sum.apply(context1, [5, 10]);
+sum.apply(context2, [5, 10]);
+
